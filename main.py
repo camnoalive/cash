8c59f2e5784b396099c17454ea44842868d8efae
Cameron Marshall <81721219+cmarshalldev@users.noreply.github.com>
2022-10-22 14:59:35 -0400

    first commit

---
+++ main.py
@@ -0,0 +1,24 @@
+import os
+import subprocess 
+os.system('cls')
+
+while True:
+    wd = subprocess.check_output("echo %cd%", shell=True);
+    command = input(f"{wd} cash$> ")
+    while command != "exit":
+        #command 1
+        if command == "cam":
+            os.system('cls')
+            break
+        #command 2
+        if command == "":
+            break
+        #other commands
+        else:
+            os.system(command)
+            break
+        
+            
+        
+    if command == "exit":
+        break
